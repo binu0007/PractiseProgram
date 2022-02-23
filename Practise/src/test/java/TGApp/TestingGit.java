2d18cfe1770822e27d0817efaeb4cecb065e09cf
binu0007 <binu.k@truglobal.com>
2022-02-23 13:06:25 +0530

    modified

---
+++ Practise/src/test/java/TGApp/TestingGit.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package TGApp;
+
+/**
+ * @author TG1670
+ *
+ */
+public class TestingGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing git hub ");
+		System.out.println("Checking modified only for fum furpose");
+	}
+	
+
+}
